1dc60a18f8684771dc775b82ad2718d4128122c4
rbobillot <raphael.bobillot@cpexterne.org>
2019-07-05 23:45:56 +0200

    adding an example of Go random wizards generator (giving some useful tools examples)

---
+++ generate_random_wizards.go
@@ -0,0 +1,136 @@
+// main is a standalone package,
+// it is used to generate and print
+// random wizards names
+// its functions (except 'main', of course)
+// should be used to "create Magic Life"
+// -> well, for the MAGIC CRUD...
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/satori/go.uuid" // go get github.com/satori/go.uuid
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+// Name is one of the Identity components
+type Name struct {
+	Title string
+	First string
+	Last  string
+}
+
+// Identity is the type contains in "results"
+type Identity struct {
+	Name Name
+}
+
+// Info has some infos about the result page,
+// it is not relevant
+type Info struct {
+	Seed    string
+	Results float64
+	Page    float64
+	Version string
+}
+
+// Names is the whole JSON content for generated names
+type Names struct {
+	Results []Identity
+	Info      Info
+}
+
+// Wizard is the content for the Magic Inventory DB
+type Wizard struct {
+	ID        string  `json:"id"`
+	FirstName string  `json:"first_name"`
+	LastName  string  `json:"last_name"`
+	Age       float64 `json:"age"`
+	Category  string  `json:"category"` // Families, Guests, Villains
+	Arrested  bool    `json:"arrested"`
+}
+
+// Any checks if id of a new Wizard doesn't already exist
+func Any(id string, wizards []Wizard) bool {
+	for _, w := range wizards {
+		if w.ID == id {
+			return true
+		}
+	}
+	return false
+}
+
+// AddWizard adds a new one only if its generated id
+// does not already belong to another created Wizard
+func AddWizard(categories []string, name Name, wizards []Wizard) []Wizard {
+	wizard := Wizard{uuid.Must(uuid.NewV4()).String(), //GenerateRandomID(32),
+			strings.Title(name.First),
+			strings.Title(name.Last),
+			float64(rand.Int() % 20 + 20),
+			categories[rand.Int() % 3],
+			false}
+
+	if Any(wizard.ID, wizards) {
+		return AddWizard(categories, name, wizards)
+	}
+
+	return append(wizards, wizard)
+}
+
+// GenerateWizards is the function that will generate
+// random wizard identities, from GetRandomNames
+func GenerateWizards(body []byte) (wizards []Wizard, err error) {
+	names := Names{}
+	categories := []string{"Families","Guests","Villains"}
+	err = json.Unmarshal(body, &names)
+
+	if err != nil {
+		return wizards, err
+	}
+
+	for _, name := range names.Results {
+		wizards = AddWizard(categories, name.Name, wizards)
+	}
+
+	return wizards, err
+}
+
+// GetRandomNames build a custom URL
+// and print JSON data via a GET request
+// no real error handling has been done
+func GetRandomNames(qty int) (body []byte, errs []error) {
+	wizardsQty := strconv.Itoa(qty)
+	wizardsOrigin := "gb" // "gb,fr,us" ...
+	format := "pretty"
+
+	url := "https://randomuser.me/api/" +
+		"?results=" + wizardsQty +
+		"&nat=" + wizardsOrigin +
+		"&format=" + format +
+		"&exc=gender,location,email,login,registered,dob,phone,cell,picture,nat,id"
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return body, append(errs, err)
+	}
+
+	defer func () {
+		err = resp.Body.Close()
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}()
+
+	body, err = ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return body, append(errs, err)
+	}
+
+	return body, nil
+}
