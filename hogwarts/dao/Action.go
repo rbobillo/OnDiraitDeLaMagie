8379f9930e6236d1aab9523059c92e4e609b7cce
artemys <aurore.012@hotmail.fr>
2019-09-03 11:04:22 +0200

    [START] starting hogwarts/attack

--- hogwarts/dao/Action.go
+++ hogwarts/dao/Action.go
@@ -5,10 +5,7 @@ import uuid "github.com/satori/go.uuid"
 // Action is the content for the Hogwarts Inventory DB
 type Action struct {
 	ID         uuid.UUID `json:"id"`
-	First_name string    `json:"first_name"`
-	Last_name  string    `json:"last_name"`
-	Age        string    `json:"age"`
-	Arrested   string    `json:"arrested"`
-	Category   string    `json:"category"`
-	Dead       string    `json:"dead"`
+	Wizard 	   uuid.UUID `json:"wizard"`
+	Action     string    `json:"action"`
+	Status       string  `json:"status"`
 }
